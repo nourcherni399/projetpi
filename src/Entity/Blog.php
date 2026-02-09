3daea2288b8e7434be318b11eafb1a30b038c823
ayaandolsi <ayaandolsi11@gmail.com>
2026-02-09 16:36:17 +0100

    Fix merge conflicts in Blog entity

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -1,13 +1,8 @@
 <?php
 
 namespace App\Entity;
-<<<<<<< HEAD
-use App\Entity\Enum\TypePost;
-
-=======
 
 use App\Entity\Enum\TypePost;
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
 use App\Repository\BlogRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -24,17 +19,10 @@ class Blog
     #[ORM\Column(length: 255)]
     private ?string $titre = null;
 
-<<<<<<< HEAD
 #[ORM\Column(
     columnDefinition: "ENUM('recommandation', 'plainte', 'question', 'experience') NOT NULL"
 )]
 private string $type;
-=======
-    #[ORM\Column(
-        columnDefinition: "ENUM('recommandation', 'plainte', 'question', 'experience') NOT NULL"
-    )]
-    private string $type;
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
 
     #[ORM\Column]
     private ?bool $isPublished = null;
@@ -69,19 +57,11 @@ private string $type;
      * @var Collection<int, Commentaire>
      */
     #[ORM\OneToMany(targetEntity: Commentaire::class, mappedBy: 'blog')]
-<<<<<<< HEAD
-    private Collection $commentaire;
-
-    public function __construct()
-    {
-        $this->commentaire = new ArrayCollection();
-=======
     private Collection $commentaires;
 
     public function __construct()
     {
         $this->commentaires = new ArrayCollection();
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
     }
 
     public function getId(): ?int
@@ -97,10 +77,6 @@ private string $type;
     public function setTitre(string $titre): static
     {
         $this->titre = $titre;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -112,10 +88,6 @@ private string $type;
     public function setType(string $type): static
     {
         $this->type = $type;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -127,10 +99,6 @@ private string $type;
     public function setIsPublished(bool $isPublished): static
     {
         $this->isPublished = $isPublished;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -142,10 +110,6 @@ private string $type;
     public function setImage(string $image): static
     {
         $this->image = $image;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -157,10 +121,6 @@ private string $type;
     public function setIsUrgent(?bool $isUrgent): static
     {
         $this->isUrgent = $isUrgent;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -172,10 +132,6 @@ private string $type;
     public function setIsVisible(bool $isVisible): static
     {
         $this->isVisible = $isVisible;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -187,10 +143,6 @@ private string $type;
     public function setDateCreation(\DateTime $dateCreation): static
     {
         $this->dateCreation = $dateCreation;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -202,10 +154,6 @@ private string $type;
     public function setDateModif(\DateTime $dateModif): static
     {
         $this->dateModif = $dateModif;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -217,10 +165,6 @@ private string $type;
     public function setContenu(string $contenu): static
     {
         $this->contenu = $contenu;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -232,10 +176,6 @@ private string $type;
     public function setModule(?Module $module): static
     {
         $this->module = $module;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
@@ -247,60 +187,34 @@ private string $type;
     public function setUser(?User $user): static
     {
         $this->user = $user;
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
     /**
      * @return Collection<int, Commentaire>
      */
-<<<<<<< HEAD
-    public function getCommentaire(): Collection
-    {
-        return $this->commentaire;
-=======
     public function getCommentaires(): Collection
     {
         return $this->commentaires;
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
     }
 
     public function addCommentaire(Commentaire $commentaire): static
     {
-<<<<<<< HEAD
-        if (!$this->commentaire->contains($commentaire)) {
-            $this->commentaire->add($commentaire);
-            $commentaire->setBlog($this);
-        }
-
-=======
         if (!$this->commentaires->contains($commentaire)) {
             $this->commentaires->add($commentaire);
             $commentaire->setBlog($this);
         }
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 
     public function removeCommentaire(Commentaire $commentaire): static
     {
-<<<<<<< HEAD
-        if ($this->commentaire->removeElement($commentaire)) {
-            // set the owning side to null (unless already changed)
-=======
         if ($this->commentaires->removeElement($commentaire)) {
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
+            // set the owning side to null (unless already changed)
             if ($commentaire->getBlog() === $this) {
                 $commentaire->setBlog(null);
             }
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> 95dad675f769b1ba531a1349a5f6084dd26c4be3
         return $this;
     }
 }
