0b37472f3b864cbaf8bfb9e66e99f0c32da10fe3
ayaandolsi <ayaandolsi11@gmail.com>
2026-02-09 16:50:17 +0100

    Fix missing User import in InscritEventsRepository

--- src/Repository/InscritEventsRepository.php
+++ src/Repository/InscritEventsRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\InscritEvents;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
